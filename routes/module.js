8335534d489387529c851257f80881fa65276eed
Sergey <sergey@skyciv.com>
2020-08-16 10:13:07 +0300

    translate into russian

--- routes/module.js
+++ routes/module.js
@@ -19,35 +19,17 @@ module.exports = {
         };
 
         if (D < 0) {
-            x1 = "the equation does not have solution";
+            x1 = "Уравнение не имеет коней";
         };
          
         report = `
-            \\( \\text{1) Calculate the descriminant:}\\)
+            \\( \\text{1) Вычисление дискрименанта:}\\)
             \\[D=\\sqrt{b^{2}-4ac}=\\sqrt{`+ b + `^{2}-4·` + a + `·` + c + `}=` + D.toFixed(2) + `\\]
-            \\( \\text{2) The roots of equation:}\\)
+            \\( \\text{2) Корни уравнения:}\\)
             \\[x_{1}=`+x1.toFixed(4)+`\\]
             \\[x_{2}=`+x2.toFixed(4)+`\\]
             `
         return { "x1": x1, "x2": x2, "report": report }
-    },
-
-    printHtmltoPDF: function (param_html) {
-
-        (async () => {
-          const browser = await puppeteer.launch();
-          const page = await browser.newPage();
-        
-          await page.setContent(`<p>` + param_html.reporthtml + `</p>`)
-          await page.emulateMedia('screen')
-          //await page.goto('http://localhost:3000', {waitUntil: 'networkidle2'});
-          await page.pdf({
-            path: './app/data/report.pdf', 
-            format: 'A4'
-          });
-        
-          await browser.close();
-        })();
     }
 }
 
